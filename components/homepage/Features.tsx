f67d79ed3c3dc69b3ab2f5daec9f63b7f9f6639f
hphats <phatlee1104@gmail.com>
2024-11-24 13:15:17 +0700

    [fix] homepage feature

--- components/homepage/Features.tsx
+++ components/homepage/Features.tsx
@@ -1,69 +1,31 @@
-import Image from "next/image";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+import FeatureCard from "./FeatureCard";
 
 const Features = () => {
 	return (
-		<div className="w-full px-5">
-			<section className="wrapper bg-gray-50 py-10 space-y-10 rounded-lg border border-neutral-200 shadow-md">
+		<div className="w-full py-10 bg-gray-100 border border-neutral-200 shadow-sm">
+			<section className="wrapper flex flex-col items-center justify-center gap-5 md:gap-10">
 				<h1 className="text-3xl font-semibold">Why choose us?</h1>
-				<div className="w-full flex max-md:flex-col items-center gap-6 md:gap-10 justify-center flex-wrap">
-					<Card className="w-80 cursor-pointer hover:scale-105 transition-transform hover:shadow-md">
-						<CardHeader className="w-full text-center">
-							<CardTitle className="text-2xl">Affordable Pricing</CardTitle>
-							<CardDescription className="text-base">
-								Best value for your money
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="w-full">
-							<Image
-								src={"/feature1.svg"}
-								alt="feature1"
-								width={500}
-								height={500}
-								className="w-full object-cover h-full"
-							/>
-						</CardContent>
-					</Card>
-					<Card className="w-80 cursor-pointer hover:scale-105 transition-transform hover:shadow-md">
-						<CardHeader className="w-full text-center">
-							<CardTitle className="text-2xl">Expert Guides</CardTitle>
-							<CardDescription className="text-base">
-								Professional and knowledgeable travel experts
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="w-full">
-							<Image
-								src={"/feature2.svg"}
-								alt="feature2"
-								width={500}
-								height={500}
-								className="w-full object-cover h-full"
-							/>
-						</CardContent>
-					</Card>
-					<Card className="w-80 cursor-pointer hover:scale-105 transition-transform hover:shadow-md">
-						<CardHeader className="w-full text-center">
-							<CardTitle className="text-2xl">24/7 Support</CardTitle>
-							<CardDescription className="text-base">
-								We’re always here to help
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="w-full">
-							<Image
-								src={"/feature3.svg"}
-								alt="feature3"
-								width={500}
-								height={500}
-								className="w-full object-cover h-full"
-							/>
-						</CardContent>
-					</Card>
+
+				{/* Features */}
+				<div className="flex gap-5 md:gap-10 max-md:flex-col flex-wrap items-center justify-center">
+					<FeatureCard
+						title="Affordable Pricing"
+						subtitle="Best value for your money"
+						image_url="/feature1.svg"
+						alt="feature1"
+					/>
+					<FeatureCard
+						title="Expert Guides"
+						subtitle="Professional and knowledgeable travel experts"
+						image_url="/feature2.svg"
+						alt="feature2"
+					/>
+					<FeatureCard
+						title="24/7 Support"
+						subtitle="We’re always here to help"
+						image_url="/feature3.svg"
+						alt="feature3"
+					/>
 				</div>
 			</section>
 		</div>
